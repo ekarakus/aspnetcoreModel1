b7523da667c0e2f99ff488de5b44a93b863427d4
Ergün Karakuş <ekarakus@btofis.com>
2019-05-16 20:59:27 +0300

    ilk

---
+++ aspnetcoreModel/Models/oyuncu.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace aspnetcoreModel.Models
+{
+    public class Oyuncu
+    {
+        public string ad { get; set; }
+        public string soyad { get; set; }
+        public string cinsiyet { get; set; }
+        public DateTime dogumTarih { get; set; }
+    }
+}
